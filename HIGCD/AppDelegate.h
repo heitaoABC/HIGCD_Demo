b5865d287b8acf0e524d2c099e6940ff02d09442
HomerIce <HomerIce@wangyuanhongdemacbook-pro.local>
2016-03-07 15:52:34 +0800

    Initial Commit

---
+++ HIGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HIGCD
+//
+//  Created by HomerIce on 16/3/7.
+//  Copyright © 2016年 HomerIce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HIGCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HIGCD
+//
+//  Created by HomerIce on 16/3/7.
+//  Copyright © 2016年 HomerIce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
